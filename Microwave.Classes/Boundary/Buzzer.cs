d3b93fc51f4a980f4a38cc555a001a31e28ff962
MadsBorkNees <79922518+MadsBorkNees@users.noreply.github.com>
2022-04-07 13:40:24 +0200

    Added buzzer class and prototype buzz-method

---
+++ Microwave.Classes/Boundary/Buzzer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microwave.Classes.Boundary
+{
+    public class Buzzer
+    {
+        private Output BuzzerOutput { get; set; }
+
+
+        public Buzzer(Output output)
+        {
+            BuzzerOutput = output;
+        }
+
+        public void Buzz(int numOfBursts, int delay)
+        {
+            for (int i = 0; i < numOfBursts; i++)
+            {
+                BuzzerOutput.OutputLine("Bzzzzzzzzzzz");
+                Thread.Sleep(delay);
+            }
+        }
+    }
+}
