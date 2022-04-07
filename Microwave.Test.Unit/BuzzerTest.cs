4931b16876a3ca4524833c7ac9b845373921f5df
MadsBorkNees <79922518+MadsBorkNees@users.noreply.github.com>
2022-04-07 14:07:12 +0200

    Created BuzzerTest

---
+++ Microwave.Test.Unit/BuzzerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microwave.Classes.Boundary;
+using NUnit.Framework;
+
+namespace Microwave.Test.Unit
+{
+    [TestFixture]
+    public class BuzzerTest
+    {
+        private Buzzer uut;
+    }
+}
