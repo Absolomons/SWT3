dfaec119e72f24cfb2ea2533a9fb52eab4c1cada
MadsBorkNees <79922518+MadsBorkNees@users.noreply.github.com>
2022-04-20 15:11:45 +0200

    Added buzzer to userinterface test

--- Microwave.Test.Unit/UserInterfaceTest.cs
+++ Microwave.Test.Unit/UserInterfaceTest.cs
@@ -21,6 +21,7 @@ namespace Microwave.Test.Unit
         private ILight light;
 
         private ICookController cooker;
+        private IBuzzer buzzer;
 
         [SetUp]
         public void Setup()
@@ -32,13 +33,15 @@ namespace Microwave.Test.Unit
             light = Substitute.For<ILight>();
             display = Substitute.For<IDisplay>();
             cooker = Substitute.For<ICookController>();
+            buzzer = Substitute.For<IBuzzer>();
 
             uut = new UserInterface(
                 powerButton, timeButton, startCancelButton,
                 door,
                 display,
                 light,
-                cooker);
+                cooker,
+                buzzer);
         }
 
         [Test]
