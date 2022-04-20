6504699abec7e6be09de4b10ed8baa157046b73b
MadsBorkNees <79922518+MadsBorkNees@users.noreply.github.com>
2022-04-20 14:42:51 +0200

    Added Buzzer test setup

--- Microwave.Test.Unit/BuzzerTest.cs
+++ Microwave.Test.Unit/BuzzerTest.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microwave.Classes.Boundary;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Microwave.Test.Unit
@@ -12,5 +14,13 @@ namespace Microwave.Test.Unit
     public class BuzzerTest
     {
         private Buzzer uut;
+        private IOutput BuzzerOutput;
+
+        [SetUp]
+        public void Setup()
+        {
+            BuzzerOutput = Substitute.For<IOutput>();
+            uut = new Buzzer(BuzzerOutput);
+        }
     }
 }
